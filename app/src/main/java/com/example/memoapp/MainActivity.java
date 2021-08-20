be7619ca57c23c1bb24bf0f001da4926a65d39f7
waroad <fda@gmail.com>
2021-08-20 16:11:41 +0900

    1

---
+++ app/src/main/java/com/example/memoapp/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.memoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class MainActivity extends Activity {
+    SQLiteDatabase sd;
+    Cursor c;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        sd=openOrCreateDatabase("memo.db",0,null);
+
+        String schema = "create table note (_id integer primary key autoincrement, title text not null, body text not null);";
+        try {
+            sd.execSQL(schema);
+        }catch (Exception ignore){
+            Log.d("%%%%%%%%","db error");
+        }
+        ContentValues values = new ContentValues();
+        values.put("title","SS");
+        values.put("body","ㅎㅇ");
+        sd.insert("note",null,values);
+        setContentView(R.layout.mainactivity);
+        ListView list = findViewById(R.id.list);
+        c = sd.query("note",null,null,null,null,null,null);
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.line,c,new String[]{"_id","title"},new int[]{R.id._id,R.id.title});
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this,SubActivity.class);
+                startActivity(intent);
+            }
+        });
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                return true;
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/memoapp/SubActivity.java
@@ -0,0 +1,15 @@
+package com.example.memoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SubActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+    }
+}
